2c41318261a70c41fed00fe89a0ec9f1ba03c227
Sergei <s.andreev.it@gmail.com>
2026-03-03 08:32:37 +0100

    feat: implement fs/findByExt task

--- src/fs/findByExt.js
+++ src/fs/findByExt.js
@@ -1,7 +1,27 @@
+import { access, readdir, stat } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
 const findByExt = async () => {
-  // Write your code here
-  // Recursively find all files with specific extension
-  // Parse --ext CLI argument (default: .txt)
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const rootPath = join(__dirname, "workspace");
+
+  try {
+    await access(rootPath);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
+
+  const extension = process.argv[3] || "txt";
+
+  const files = await readdir(rootPath, { recursive: true });
+
+  const sortedFilesWithExtension = files.filter((file) => file.endsWith(extension)).sort();
+  
+  for (const file of sortedFilesWithExtension) {
+    console.log(file);
+  }
 };
 
 await findByExt();
