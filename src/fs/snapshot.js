4bcad2fd466387aa508d3ac5c22c4d7ed2ca5a98
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2026-03-03 12:14:27 +0400

    feat: implement snapshot

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,9 +1,60 @@
+import { writeFile, readdir, stat, readFile } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workspaceDir = path.resolve(__dirname, "../../workspace");
+
+async function scanDir(absDir, relativeDir = "") {
+  const entries = await readdir(absDir, { withFileTypes: true });
+
+  const contentEntries = [];
+
+  for (const entry of entries) {
+    const absPath = path.resolve(absDir, entry.name);
+    const relativePath = relativeDir
+      ? path.join(relativeDir, entry.name)
+      : entry.name;
+
+    if (entry.isDirectory()) {
+      contentEntries.push({
+        path: relativePath,
+        type: "directory",
+      });
+
+      const nested = await scanDir(absPath, relativePath);
+      contentEntries.push(...nested);
+    } else if (entry.isFile()) {
+      const fileStat = await stat(absPath);
+      const buffer = await readFile(absPath);
+
+      contentEntries.push({
+        path: relativePath,
+        type: "file",
+        size: fileStat.size,
+        content: buffer.toString("base64"),
+      });
+    }
+  }
+  return contentEntries;
+}
+
 const snapshot = async () => {
-  // Write your code here
-  // Recursively scan workspace directory
-  // Write snapshot.json with:
-  // - rootPath: absolute path to workspace
-  // - entries: flat array of relative paths and metadata
+  const entries = await scanDir(workspaceDir);
+  const content = {
+    rootPath: workspaceDir,
+    entries,
+  };
+  try {
+    await writeFile(
+      "./snapshot.json",
+      JSON.stringify(content, null, 2),
+      "utf8",
+    );
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await snapshot();
