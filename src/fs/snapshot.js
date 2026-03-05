8230b6ea01313da37d2ed60c43dc9493be904532
Jahongir Hayitov <Jahongirhacking@gmail.com>
2026-03-05 14:09:27 +0500

    fix: correct mistakes

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -3,50 +3,60 @@ import path from "node:path";
 import process from "node:process";
 
 const snapshot = async () => {
-  const restoredFolderName = "workspace_restored";
-  const root = process.cwd();
-  const files = await fs.readdir(process.cwd(), {
-    recursive: true,
-    withFileTypes: true,
-  });
-  const entries = await Promise.all(
-    files?.map(async (file) => {
-      const fullPath = path.join(file?.parentPath, file?.name);
-      const fileStat = await fs.stat(fullPath);
-      let fileContent = "";
-      try {
-        if (file?.isFile()) {
-          fileContent = await fs.readFile(fullPath);
-        }
-      } catch (err) {
-        console.error("Cannot read file:", fullPath, err.message);
+  try {
+    const workspace = path.join(process.cwd(), "workspace");
+    try {
+      if (!(await fs.stat(workspace)).isDirectory()) {
+        throw new Error("workspace folder is not created");
       }
+    } catch {
+      throw new Error("workspace folder is not created");
+    }
+    const files = await fs.readdir(workspace, {
+      recursive: true,
+      withFileTypes: true,
+    });
+    const entries = await Promise.all(
+      files?.map(async (file) => {
+        const fullPath = path.join(file?.parentPath, file?.name);
+        const fileStat = await fs.stat(fullPath);
+        let fileContent = "";
+        try {
+          if (file?.isFile()) {
+            fileContent = await fs.readFile(fullPath);
+          }
+        } catch (err) {
+          console.error("Cannot read file:", fullPath, err.message);
+        }
 
-      return {
-        path: path.relative(root, fullPath),
-        type: file?.isFile() ? "file" : "directory",
-        ...(file?.isFile()
-          ? {
-              size: fileStat.size,
-              content: fileContent ? fileContent.toString("base64") : "",
-            }
-          : {}),
-      };
-    }),
-  );
-  fs.writeFile(
-    "./snapshot.json",
-    JSON.stringify(
-      {
-        rootPath: root,
-        entries: entries?.filter((e) => !e?.path?.includes(restoredFolderName)),
-      },
-      null,
-      2,
-    ),
-    "utf-8",
-  );
-  console.log("snapshot.json is created successfully!");
+        return {
+          path: path.relative(workspace, fullPath),
+          type: file?.isFile() ? "file" : "directory",
+          ...(file?.isFile()
+            ? {
+                size: fileStat.size,
+                content: fileContent ? fileContent.toString("base64") : "",
+              }
+            : {}),
+        };
+      }),
+    );
+    fs.writeFile(
+      "./snapshot.json",
+      JSON.stringify(
+        {
+          rootPath: process.cwd(),
+          entries,
+        },
+        null,
+        2,
+      ),
+      "utf-8",
+    );
+    console.log("snapshot.json is created successfully!");
+  } catch (err) {
+    console.error(`FS operation failed\n`, err);
+  }
 };
 
 await snapshot();
