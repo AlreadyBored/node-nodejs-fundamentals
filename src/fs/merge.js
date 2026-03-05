857ea098fd515994f013b173e361569255c6c1da
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-03-05 05:05:11 +0500

    feat: fs 2 task

--- src/fs/merge.js
+++ src/fs/merge.js
@@ -1,8 +1,50 @@
+import path from 'path';
+import fs from 'fs';
+
 const merge = async () => {
-  // Write your code here
-  // Default: read all .txt files from workspace/parts in alphabetical order
-  // Optional: support --files filename1,filename2,... to merge specific files in provided order
-  // Concatenate content and write to workspace/merged.txt
+  const dir = './src/cp';
+  const argvs = process.argv.slice(2);
+  let listFiles = [];
+
+  try {
+    if (argvs[0] === '--files' && argvs[1]) {
+      listFiles = process.argv[1].split(',');
+
+      for (const file of listFiles) {
+        console.log(dir, file.name)
+        fs.accessSync(path.join(dir, file.name));
+      }
+    } else {
+      const files = fs.readdirSync(dir, { withFileTypes: true });
+
+      listFiles = files
+        .filter((file) => {
+          const fullPath = file.parentPath + '/' + file.name;
+          const stat = fs.statSync(fullPath);
+          console.log(fullPath)
+          if (stat.isFile()
+            && path.basename(fullPath).split('.')[path.basename(fullPath).split('.').length - 1] === 'js') {
+              return file;
+          }
+        }).sort();
+
+        if (listFiles.length === 0) {
+          console.error('No text file found!');
+        }
+    }
+
+    for (const file of listFiles) {
+      const content = fs.readFileSync(path.join(dir, file.name), 'utf-8');
+      process.stdout.write(content + '\n');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
 
 await merge();
+
+processFiles().catch(err => {
+    console.error(err.message);
+    process.exit(1);
+});
