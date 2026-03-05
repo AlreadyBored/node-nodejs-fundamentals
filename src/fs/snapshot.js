f67aaedda36ac5239517b3e3330b819640126b8f
Forlocks <volgaopa@gmail.com>
2026-03-05 06:46:40 +0300

    init: start task

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,9 +1,42 @@
+import { resolve, join } from 'node:path';
+import { readdir, readFile, lstat } from 'node:fs/promises';
+
 const snapshot = async () => {
-  // Write your code here
-  // Recursively scan workspace directory
-  // Write snapshot.json with:
-  // - rootPath: absolute path to workspace
-  // - entries: flat array of relative paths and metadata
+  const getDirEntries = async (path) => {
+    const dirEntries = await readdir(path, { withFileTypes: true });
+
+    return await Promise.all(dirEntries.map(async entry => {
+      const entryInfo = { path: entry.name };
+      const fullPath = join(path, entryInfo.path);
+
+      if (entry.isFile()) {
+        const stats = await lstat(fullPath);
+
+        entryInfo.type = 'file';
+        entryInfo.size = stats.size;
+        entryInfo.content = await readFile(fullPath, 'utf-8');
+        
+        return entryInfo;
+      }
+
+      entryInfo.type = 'directory';
+
+      const subDirEntries = await getDirEntries(fullPath);
+
+      return [
+        entryInfo,
+        subDirEntries.flatMap(element => ({
+          ...element,
+          path: join(entryInfo.path, element.path),
+        })),
+        ];
+    }));
+  };
+
+  const rootPath = resolve('workspace');
+  const entries = await getDirEntries(rootPath);
+
+  console.log({ rootPath, entries: entries.flat(Infinity) });
 };
 
 await snapshot();
