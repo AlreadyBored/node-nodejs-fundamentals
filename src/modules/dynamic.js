26a9738aced923fcdeb5d0271c6a5ef5349d715b
Vitaliy Bilous <vital.gm.dev@gmail.com>
2026-03-05 21:58:35 +0100

    feat: modules/dynamic is added

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -1,9 +1,32 @@
+import { parseArgs } from "node:util";
+
 const dynamic = async () => {
   // Write your code here
   // Accept plugin name as CLI argument
   // Dynamically import plugin from plugins/ directory
   // Call run() function and print result
   // Handle missing plugin case
+
+  const pluginName = parseArgs({ allowPositionals: true }).positionals[0];
+
+  if (!pluginName) {
+    console.error("Plugin not found");
+    process.exit(1);
+  }
+
+  try {
+    const plugin = await import(`./plugins/${pluginName}.js`);
+
+    if (plugin && typeof plugin.run === "function") {
+      const result = plugin.run();
+
+      console.log(result);
+    } else {
+      console.error("No such function 'run()' for plugin:", pluginName);
+    }
+  } catch (error) {
+    console.error("No such plugin:", error.message);
+  }
 };
 
 await dynamic();
