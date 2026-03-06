eed29b616a56772fe8095ae6a2ebf72f6e4f916d
GSerdg <grebelsergey@mail.ru>
2026-03-06 11:51:50 +0300

    feat: add lineNumberer

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,31 @@
+import { stdin as input, stdout as output } from "node:process";
+import { Transform, pipeline } from "node:stream";
+
 const lineNumberer = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Use Transform Stream to prepend line numbers
-  // Write to process.stdout
+  let lineNumber = 1;
+
+  const transform = new Transform({
+    transform(chunk, _, callback) {
+      console.log('chunk', chunk.toString())
+      const tmp = chunk.toString();
+
+      const lines = tmp.split("\n");
+
+      for (const line of lines) {
+        this.push(`${lineNumber++} | ${line}\n`);
+      }
+
+      callback();
+    },
+  });
+
+  pipeline(input, transform, output, (err) => {
+    if (err) {
+      console.error("Pipeline failed.", err);
+    } else {
+      console.log("Pipeline succeeded.");
+    }
+  });
 };
 
 lineNumberer();
