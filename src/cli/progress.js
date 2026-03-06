e9ae13d2312a04d93a0603399cf3bee4d2dbea40
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-03-06 23:55:25 +0500

    fix: value not throw err

--- src/cli/progress.js
+++ src/cli/progress.js
@@ -1,4 +1,5 @@
 import { parseArgs } from 'node:util';
+import { hexToAsnii, validateValueType } from '../utils/util.js';
 
 const progress = () => {
   const moderatedArgv = process.argv.slice(2);
@@ -17,12 +18,15 @@ const progress = () => {
         type: 'string',
         default: '30',
       },
-      color: {
-        type: 'string',
-        default: '#FFFFFF',
-      }
-    }
+    },
+    strict: false,
+    allowPositionals: true
   })
+  if (values.color) {
+    values.color = moderatedArgv[moderatedArgv.indexOf('--color') + 1];
+  }
+  
+  console.log(values.color)
   validateValueType(values);
 
   const fullWidth = parseInt(values.length);
@@ -49,31 +53,3 @@ const progress = () => {
 };
 
 progress();
-
-function validateValueType(argsKeyValueForm) {
-  
-  if (isNaN(argsKeyValueForm.duration)) {
-    throw new Error('No valid duration value!')
-  }
-
-  if (isNaN(argsKeyValueForm.interval)) {
-    throw new Error('No valid interval value!')
-  }
-
-  if (isNaN(argsKeyValueForm.length)) {
-    throw new Error('No valid length value!')
-  }
-
-  const regHex = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i
-
-  if (!regHex.test(argsKeyValueForm.color)) {
-    argsKeyValueForm.color = '#FFFFFF';
-  }
-}
-
-function hexToAsnii(hexString) {
-  const red = parseInt(hexString.slice(1, 3), 16);
-  const green = parseInt(hexString.slice(3, 5), 16);
-  const blue = parseInt(hexString.slice(5), 16);
-  return `\x1b[38;2;${red};${green};${blue}m`;
-}
--- src/utils/util.js
+++ src/utils/util.js
@@ -42,3 +42,30 @@ export const keyMerge = (sortedArr) => {
 
     return result;
 }
+
+export const hexToAsnii = (hexString) => {
+    const red = parseInt(hexString.slice(1, 3), 16);
+    const green = parseInt(hexString.slice(3, 5), 16);
+    const blue = parseInt(hexString.slice(5), 16);
+    return `\x1b[38;2;${red};${green};${blue}m`;
+}
+
+export const validateValueType = (argsKeyValueForm) => {
+    if (isNaN(argsKeyValueForm.duration)) {
+        process.stderr.write(new Error('No valid duration value!'))
+    }
+
+    if (isNaN(argsKeyValueForm.interval)) {
+        process.stderr.write(new Error('No valid interval value!'));
+    }
+
+    if (isNaN(argsKeyValueForm.length)) {
+        process.stderr.write(new Error('No valid length value!'));
+    }
+
+    const regHex = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i
+
+    if (!regHex.test(argsKeyValueForm.color)) {
+        argsKeyValueForm.color = '#FFFFFF';
+    }
+}
