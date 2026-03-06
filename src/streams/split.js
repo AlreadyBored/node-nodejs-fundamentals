79c98564b66d7f7e03d633b9be3b489572d8c104
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-03-06 08:36:17 +0500

    feat: last task stream

--- src/streams/split.js
+++ src/streams/split.js
@@ -1,8 +1,48 @@
+import { parseArgs } from 'node:util';
+import readline from 'readline';
+import fs from 'node:fs';
+const splitArgv = process.argv.slice(2);
+const { values } = parseArgs({
+  splitArgv,
+  options: {
+    lines: {
+      type: 'string',
+      default: '10',
+    }
+  }
+});
+
 const split = async () => {
-  // Write your code here
-  // Read source.txt using Readable Stream
-  // Split into chunk_1.txt, chunk_2.txt, etc.
-  // Each chunk max N lines (--lines CLI argument, default: 10)
+  const dir = process.cwd() + '/source.txt';
+  try {
+    // Readable Stream, I didnot get that is it okay fs.createReadStream or I need to use from Web Stream
+    const readableStream = fs.createReadStream(dir, { encoding: 'utf8' });
+    
+    const rl = readline.createInterface({
+      input: readableStream,
+      crlfDelay: Infinity
+    });
+
+    let fileCnt = 1;
+    let lineCnt = 0;
+    let writebleStream = null;
+
+    for await (const line of rl) {
+      if (lineCnt % parseInt(values.lines) === 0) {
+        if (writebleStream) {
+          writebleStream.end();
+        }
+        writebleStream = fs.createWriteStream(`chunk_${fileCnt++}.txt`);
+      }
+
+      writebleStream.write(`${line}\n`);
+      lineCnt++;
+    }
+
+    if (writebleStream) writebleStream.end();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await split();
