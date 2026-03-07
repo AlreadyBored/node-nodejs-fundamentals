ecfc1651c5952e61ffbf568257555bc78baf6e81
Andrii Mosin <mosin.andrii@flawless.froup>
2026-03-07 19:50:33 +0200

    feat: cp implementation

--- src/cp/execCommand.js
+++ src/cp/execCommand.js
@@ -1,10 +1,24 @@
+import { spawn } from "node:child_process";
+
 const execCommand = () => {
-  // Write your code here
-  // Take command from CLI argument
-  // Spawn child process
-  // Pipe child stdout/stderr to parent stdout/stderr
-  // Pass environment variables
-  // Exit with same code as child
+  const commandString = process.argv[2];
+
+  if (!commandString) {
+    console.error("No command provided");
+    process.exit(1);
+  }
+
+  const child = spawn(commandString, [], {
+    env: process.env,
+    shell: true,
+  });
+
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+
+  child.on("close", (code) => {
+    process.exit(code);
+  });
 };
 
 execCommand();
