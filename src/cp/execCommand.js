a268695543d67c412f6bd3449fe1e4412ee27490
Forlocks <volgaopa@gmail.com>
2026-03-08 23:47:57 +0300

    feat: add execcommand function

--- src/cp/execCommand.js
+++ src/cp/execCommand.js
@@ -1,10 +1,30 @@
+import { spawn } from 'node:child_process';
+
 const execCommand = () => {
-  // Write your code here
-  // Take command from CLI argument
-  // Spawn child process
-  // Pipe child stdout/stderr to parent stdout/stderr
-  // Pass environment variables
-  // Exit with same code as child
+  const { argv, exit, env } = process;
+
+  const commandArg = argv[2];
+
+  if (!commandArg) {
+    console.error('Command not specified.');
+    exit(1);
+  }
+
+  const [command, ...args] = commandArg.split(' ');
+
+  const child = spawn(command, args, {
+    stdio: 'inherit',
+    env,
+  });
+
+  child.on('exit', (code) => {
+    exit(code);
+  });
+
+  child.on('error', (err) => {
+    console.error('The command execution failed with an error', err.message);
+    exit(1);
+  });
 };
 
 execCommand();
