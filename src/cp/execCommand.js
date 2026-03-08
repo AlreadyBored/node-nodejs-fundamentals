fa9a922481e7795e8ed388186446b53a17664093
Vitaliy Bilous <vital.gm.dev@gmail.com>
2026-03-08 20:13:02 +0100

    feat: execCommand done

--- src/cp/execCommand.js
+++ src/cp/execCommand.js
@@ -1,3 +1,6 @@
+import { spawn } from "child_process";
+import { pipeline } from "stream";
+
 const execCommand = () => {
   // Write your code here
   // Take command from CLI argument
@@ -5,6 +8,27 @@ const execCommand = () => {
   // Pipe child stdout/stderr to parent stdout/stderr
   // Pass environment variables
   // Exit with same code as child
+
+  const command = process.argv[2];
+
+  const child = spawn(command, {
+    shell: true,
+    stdio: "pipe", // wiht 'inherit' it even simplier
+    env: process.env,
+  });
+
+  pipeline(child.stdout, process.stdout, (err) => {
+    if (err) console.error("Stdout pipeline failed", err);
+  });
+  pipeline(child.stderr, process.stderr, (err) => {
+    if (err) console.error("Stderr pipeline failed", err);
+  });
+
+  child.on("exit", (code) => {
+    child.stdout.destroy();
+    child.stderr.destroy();
+    process.exit(code);
+  });
 };
 
 execCommand();
