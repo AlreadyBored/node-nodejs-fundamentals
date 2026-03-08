aa2cd2d735c3409382dc3c3c1778f8fdc0f7a651
stsTom <tamilaustsiapan@gmail.com>
2026-03-08 09:33:19 +0100

    Implemented searching by file name

--- src/fs/merge.js
+++ src/fs/merge.js
@@ -1,6 +1,4 @@
-import console from "console";
 import fs from "fs/promises";
-import { connect } from "http2";
 import path from "path";
 
 const merge = async () => {
@@ -8,36 +6,72 @@ const merge = async () => {
   // Default: read all .txt files from workspace/parts in alphabetical order
   // Optional: support --files filename1,filename2,... to merge specific files in provided order
   // Concatenate content and write to workspace/merged.txt
+  // const workspace = path.join()
   const workspace = path.join(process.cwd(), 'workspace')
   const partsPath = path.join(workspace, 'parts')
-  
-  var textFiles = []
+
+  const txtCheck = async () => {
+    const files = await fs.readdir(partsPath)
+    try{
+      if (!files.some(f => path.extname(f) === '.txt')){
+        throw 'No .txt files'
+      }
+    }catch (err){
+      if (err.message === 'No .txt files') throw "FS operation failed"
+    }
+  }
+
+  txtCheck()
+
+  var textContents = []
   var merged = ""
   
-  const putTxt = async dir => {
+  const searchByFileName = async (names, dir) => {
+    for (let n of names){
+      const files = await fs.readdir(dir)
+      if(!files.includes(n)){
+        console.log( path.parse(n).name )
+        throw "FS operation failed"
+      }
+      const filePath = path.join(dir, n)
+      textContents.push(await fs.readFile(filePath, 'utf8'))
+    }
+  }
+
+  const searchByExtension = async dir => {
     for (let f of await fs.readdir(dir)){
       const filePath = path.join(dir, f)
       const stats = await fs.stat(filePath)
-
+      
       if (stats.isDirectory()){
-        await putTxt(filePath)
-      }else if (path.extname(f) != ".txt"){ //there is a function that checks if every element pass the test 
+        continue
+      }
+
+      if (path.extname(f) != ".txt"){
         throw "FS operation failed"
       }else{
         const fileData = await fs.readFile(filePath, 'utf8')
-        textFiles.push(fileData)
+        textContents.push(fileData)
       }
     }
   }
   
   const mergeText = async () => {
-    for (let i of textFiles){
+    for (let i of textContents){
       merged = merged.concat(i, "\n")  
     }
     await fs.writeFile(path.join(workspace, 'merged.txt'), merged)
   }
 
-  await putTxt(partsPath);
+  if (process.argv.length >= 4){
+    if (process.argv.includes('--files') && process.argv.indexOf('--files') < process.argv.length-1){
+      const fileNames = process.argv.filter(a => process.argv.indexOf(a) > process.argv.indexOf('--files'))
+      await searchByFileName(fileNames, partsPath);
+    }
+  }else{
+    await searchByExtension(partsPath);
+  }
+
   await mergeText();
 };
 
