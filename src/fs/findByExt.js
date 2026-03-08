3e4d23542a22cca0b6dd7f6d303d84ce9b771edd
natasyo <natasyo23@gmail.com>
2026-03-08 12:17:56 +0300

    feat: cli:interactive

--- src/fs/findByExt.js
+++ src/fs/findByExt.js
@@ -1,4 +1,4 @@
-import fs from "fs/promises"
+import fs from "fs/promises";
 import path from "path";
 
 const findByExt = async () => {
@@ -6,34 +6,31 @@ const findByExt = async () => {
   // Recursively find all files with specific extension
   // Parse --ext CLI argument (default: .txt)
 
-
-  const root=process.cwd();
-  const filesArray=[]
-    async function scanDir(ext, dir){
-      const files=await fs.readdir(dir);
-      for(let file of files){
-        const fullPath=path.join(dir, file)
-        const stat=await fs.stat(fullPath)
-        if(stat.isDirectory()) await scanDir(ext, fullPath)
-          else{
-          if(path.extname(file).replace('.','')===ext){
-            filesArray.push(path.relative(root, fullPath))
-          }
+  const root = process.cwd();
+  const filesArray = [];
+  async function scanDir(ext, dir) {
+    const files = await fs.readdir(dir);
+    for (let file of files) {
+      const fullPath = path.join(dir, file);
+      const stat = await fs.stat(fullPath);
+      if (stat.isDirectory()) await scanDir(ext, fullPath);
+      else {
+        if (path.extname(file).replace(".", "") === ext) {
+          filesArray.push(path.relative(root, fullPath));
         }
       }
     }
-
-  try{
-     const index=process.argv.indexOf('--ext');
-     if(index <0) throw new Error('Not found')
-     const ext=process.argv[index+1];
- await scanDir(ext, root);
-  console.log(filesArray)
-
-  }catch(e){
-    console.error('Error\n',"FS operation failed", '\n\n')
   }
- 
+
+  try {
+    const index = process.argv.indexOf("--ext");
+    if (index < 0) throw new Error("Not found");
+    const ext = process.argv[index + 1];
+    await scanDir(ext, root);
+    console.log(filesArray);
+  } catch (e) {
+    console.error("Error\n", "FS operation failed", "\n\n");
+  }
 };
 
 await findByExt();
