ce176751f19b67892d6d4fb681b2121617ef7e9d
natasyo <natasyo23@gmail.com>
2026-03-10 20:15:21 +0300

    feat: cp command

--- src/cp/execCommand.js
+++ src/cp/execCommand.js
@@ -1,4 +1,5 @@
-import { Worker } from "worker_threads";
+import { Worker, } from "worker_threads";
+import {spawn} from "child_process"
 
 function setArrayInWorker() {
   const worker = new Worker(`
@@ -13,6 +14,19 @@ const execCommand = () => {
   // Pipe child stdout/stderr to parent stdout/stderr
   // Pass environment variables
   // Exit with same code as child
+const command=process.argv[2];
+  if (!command) {
+    console.error("No command provided. Usage: node execCommand.js \"ls -la\"");
+    process.exit(1);
+  }
+const child=spawn(command,{
+  shell:true,
+  env: process.env
+})
+child.stdout.pipe(process.stdout)
+child.stdin.pipe(process.stdin)
+child.on('close', (code)=>process.exit(code))
+
 };
 
 execCommand();
